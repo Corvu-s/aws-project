6c784c5f81bad1e2b1c7a06ab440ca140711842e
lukelin <lukelin@amazon.com>
2022-07-14 15:34:14 -0400

    added a status section

---
+++ components/Alerts.js
@@ -0,0 +1,108 @@
+import AccessTimeIcon from "@mui/icons-material/AccessTime"; //Heavy Traffic
+import DoNotDisturbIcon from "@mui/icons-material/DoNotDisturb"; //no access
+import SignalWifi0BarIcon from "@mui/icons-material/SignalWifi0Bar"; // System down
+
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ImageIcon from "@mui/icons-material/Image";
+import WorkIcon from "@mui/icons-material/Work";
+import BeachAccessIcon from "@mui/icons-material/BeachAccess";
+export default function Alerts() {
+  //render a list of alerts to the status secion
+  const alertData = [
+    {
+      location: "123 Street W",
+      status: "Heavy Traffic",
+      reason: "road work",
+      lastUpdated: "now",
+    },
+    {
+      location: "456 Avenue",
+      status: "No Access",
+      reason: "Marathon",
+      lastUpdated: "5 mins ago",
+    },
+    {
+      location: "798 Cherry St",
+      status: "No Access",
+      reason: "Marathon",
+      lastUpdated: "10 mins ago",
+    },
+    {
+      location: "Block Ave",
+      status: "Heavy Traffic",
+      reason: "unknown",
+      lastUpdated: "now",
+    },
+    {
+      location: "Toronto",
+      status: "System Down",
+      reason: "Rogers...",
+      lastUpdated: "now",
+    },
+    {
+      location: "123 Street W",
+      status: "Heavy Traffic",
+      reason: "road work",
+      lastUpdated: "now",
+    },
+    {
+      location: "456 Avenue",
+      status: "No Access",
+      reason: "Marathon",
+      lastUpdated: "5 mins ago",
+    },
+    {
+      location: "798 Cherry St",
+      status: "No Access",
+      reason: "Marathon",
+      lastUpdated: "10 mins ago",
+    },
+    {
+      location: "Block Ave",
+      status: "Heavy Traffic",
+      reason: "unknown",
+      lastUpdated: "now",
+    },
+    {
+      location: "Toronto",
+      status: "System Down",
+      reason: "Rogers...",
+      lastUpdated: "now",
+    },
+  ];
+  function determineIcon(type) {
+    if (type == "System Down") {
+      return <SignalWifi0BarIcon />;
+    } else if (type == "No Access") {
+      return <DoNotDisturbIcon />;
+    } else if (type == "Heavy Traffic") {
+      return <AccessTimeIcon />;
+    }
+  }
+  return (
+    <div>
+      <List
+        sx={{
+          overflow: "auto",
+          maxHeight: 500,
+        }}
+      >
+        {alertData.map((alert, index) => (
+          <ListItem key={index} className="flex flex-wrap">
+            <ListItemAvatar>
+              <Avatar>{determineIcon(alert.status)}</Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={alert.status + " at " + alert.location}
+              secondary="Jan 9, 2014"
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
--- components/ImageCarousel.js
+++ components/ImageCarousel.js
@@ -1,26 +1,32 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-
+import Alerts from "./Alerts";
 export default function ImageCarousel() {
   //service outage image
   //welcome image
-  //news?
+  //learn about EVs
   return (
-    <div>
-      <Carousel autoPlay={true}>
+    <div className="flex space-x-2 flex-wrap">
+      <Carousel autoPlay={true} className="w-1/2 bookingForm ">
         <div>
           <img src="/placeholder.png" />
+
           <p className="legend">Legend 1</p>
         </div>
         <div>
-          <img src="/placeholder.png" />
-          <p className="legend">Legend 2</p>
+          <img src="/ev.jpg" />
+
+          <p className="legend">Learn more about of EV fleet</p>
         </div>
         <div>
-          <img src="/placeholder.png" />
+          <img src="/transit.jpg" />
           <p className="legend">Legend 3</p>
         </div>
       </Carousel>
+      <div className="altBackground w-1/3 ">
+        <p className="text-lg font-bold">Outages and Statuses</p>
+        <Alerts />
+      </div>
     </div>
   );
 }
